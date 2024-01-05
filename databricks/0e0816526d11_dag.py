2c41bcc60f0f22a462173ad0027bd57bf1744976
nikesh mistry <nikeshmistry83@gmail.com>
2024-01-05 21:34:24 +0000

    Uploaded dag

---
+++ databricks/0e0816526d11_dag.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta
+
+# Define params for Submit Run Operator
+notebook_task = {'notebook_path': '/Users/nikeshmistry83@gmail.com/batch_cleaning_and_queries_databricks',}
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+default_args = {
+    'owner': 'Nikesh Mistry',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG('0e0816526d11_dag',
+    # should be a datetime format
+    start_date=(2023, 12, 27),
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
