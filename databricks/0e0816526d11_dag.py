06274a54904465141c72b077887f42b124833a28
nikesh mistry <nikeshmistry83@gmail.com>
2024-01-09 20:57:49 +0000

    Updated the dag

--- databricks/0e0816526d11_dag.py
+++ databricks/0e0816526d11_dag.py
@@ -15,7 +15,7 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 2,
     'retry_delay': timedelta(minutes=2)
 }
 
